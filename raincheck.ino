3b579e28286286f8cee133d3476866e58e1aba3f
kasperkarlsson <kasper.karlsson@omegapoint.se>
2017-10-08 23:39:25 +0200

    Implemented custom buffered JSON parser, blink onboard LED fast if rain is expected and slow otherwise

--- raincheck.ino
+++ raincheck.ino
@@ -1,28 +1,36 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 #include "settings.h"
- 
+
+// This corresponds to the number of hours to check precipitation for,
+// e.g. 10 means "Will it rain in the next 10 hours?"
+#define VALUES_TO_PARSE 10
+
+
 int ledPin = LED_BUILTIN;
 int ledStatus = LOW;
 WiFiServer server(80);
 
-/*
- * Get preticipation value
- * 
- * https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/11.5759/lat/57.4225/data.json
- * http://opendata.smhi.se/apidocs/metfcst/parameters.html#parameters
- * 
- * YR:
- * http://www.yr.no/stad/Sverige/V%C3%A4stra_G%C3%B6taland/G%C3%B6teborg/varsel.xml
-*/
+String SMHI_URL = "http://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/11.5759/lat/57.4225/data.json";
+float precipitation[VALUES_TO_PARSE];
 
-void http_get(String url) {
-  // 
+// http://opendata.smhi.se/apidocs/metfcst/parameters.html#parameters
+
+typedef enum ParseMode{
+  parse_init,
+  parsing_key,
+  key_end,
+  parsing_value,
+  value_ended
+};
+
+bool get_precipitation() {
+  bool success = false;
   HTTPClient http;
 
   Serial.print("[HTTP] Begin - ");
-  Serial.println(url);
-  http.begin(url);
+  Serial.println(SMHI_URL);
+  http.begin(SMHI_URL);
 
   Serial.println("[HTTP] Sending GET request");
   // Start connection and send request
@@ -35,9 +43,18 @@ void http_get(String url) {
       // Create read buffer
       uint8_t buff[256] = { 0 };
       WiFiClient *stream = http.getStreamPtr();
+      // Initialize parser state machine
+      ParseMode parser = parse_init;
+      String parse_buffer = "";
 
       // Read all data from server
+      byte parsed_values = 0;
       while(http.connected() && (len > 0 || len == -1)) {
+        if (parsed_values == VALUES_TO_PARSE) {
+          success = true;
+          Serial.println("Break!");
+          break;
+        }
         // Get available data size
         size_t available_size = stream->available();
 
@@ -45,8 +62,56 @@ void http_get(String url) {
           // Process chunk
           int c = stream->readBytes(buff, ((available_size > sizeof(buff)) ? sizeof(buff) : available_size));
 
-          // TODO Parse JSON here
-          Serial.write((char*)buff);
+          // Parse JSON, looking for values in the following format:
+          // {"name":"pmax","levelType":"hl","level":0,"unit":"kg/m2/h","values":[0.0]}
+          for (int i=0; i<c; i++) {
+            char current_char = (char)buff[i];
+            //Serial.write(current_char);
+            switch (parser) {
+              case parse_init:
+                if (current_char == '"') {
+                  parser = parsing_key;
+                  parse_buffer = "";
+                }
+                break;
+              case parsing_key:
+                if (current_char == '"') {
+                  // Key parsed - check if it matches
+                  if (parse_buffer == "pmax") {
+                    parser = key_end;
+                  }
+                  else {
+                    parser = parse_init;
+                  }
+                }
+                else {
+                  // Protect against too long strings
+                  if (parse_buffer.length() < 20) {
+                    parse_buffer += current_char;
+                  }
+                }
+                break;
+              case key_end:
+                if (current_char == '[') {
+                  parser = parsing_value;
+                  parse_buffer = "";
+                }
+                break;
+              case parsing_value:
+                if (current_char == ']') {
+                  // Finished parsing value - save and continue
+                  float parsed_value = parse_buffer.toFloat();
+                  precipitation[parsed_values++] = parsed_value;
+                  parser = parse_init;
+                }
+                else {
+                  // Protect against too long values
+                  if (parse_buffer.length() < 5) {
+                    parse_buffer += current_char;
+                  }
+                }
+            }
+          }
 
           if(len > 0) {
             len -= c;
@@ -54,13 +119,23 @@ void http_get(String url) {
         }
         delay(1);
       }
-      Serial.println();
+      //Serial.println();
     }
   }
   else {
     Serial.printf("[HTTP] GET failed: %s\n", http.errorToString(httpCode).c_str());
   }
   http.end();
+  return success;
+}
+
+void blink(byte blinks, int blink_duration) {
+  for (byte i=0; i<blinks; i++) {
+    digitalWrite(ledPin, HIGH);
+    delay(blink_duration);
+    digitalWrite(ledPin, LOW);
+    delay(blink_duration);
+  }
 }
  
 void setup() {
@@ -79,10 +154,7 @@ void setup() {
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
  
   while (WiFi.status() != WL_CONNECTED) {
-    digitalWrite(ledPin, HIGH);
-    delay(250);
-    digitalWrite(ledPin, LOW);
-    delay(250);
+    blink(1, 250);
     Serial.print(".");
   }
   Serial.println("");
@@ -93,11 +165,35 @@ void setup() {
   Serial.println("Server started");
  
   // Print the IP address
-  Serial.print("Use this URL to connect: ");
+  Serial.print("Serving on ");
   Serial.print("http://");
   Serial.print(WiFi.localIP());
   Serial.println("/");
- 
+
+  // Lookup rain data
+  bool success = get_precipitation();
+  if (success) {
+    Serial.print("Lookup succeeded, values: ");
+    bool it_will_rain = false;
+    for (int i=0; i<VALUES_TO_PARSE; i++) {
+      if (precipitation[i] > 0.0) {
+        it_will_rain = true;
+      }
+      Serial.print(precipitation[i]);
+      Serial.print(" ");
+    }
+    Serial.println();
+    if (it_will_rain) {
+      Serial.println("It will rain!");
+      blink(50, 100);
+    }
+    else {
+      Serial.println("It will not rain.");
+      blink(5, 1000);
+    }
+  } else {
+    Serial.println("Lookup failed");
+  }
 }
  
 void loop() {
@@ -108,7 +204,7 @@ void loop() {
   }
  
   // Wait until the client sends some data
-  Serial.println("new client");
+  Serial.println("New client");
   while(!client.available()){
     delay(1);
   }
@@ -149,7 +245,7 @@ void loop() {
   client.println("<a href='/?action=HTTP'><button>Send HTTP request</button></a><br /><br />");  
   
   if (request.indexOf("/?action=HTTP") != -1)  {
-    http_get("http://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/11.5759/lat/57.4225/data.json");
+    get_precipitation();
   }
   
   client.println("</html>");
